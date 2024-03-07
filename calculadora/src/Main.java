9a5143b88d18296f4c9686c5780264554b30d5d2
DaviPRabelo <160641658+DaviPRabelo@users.noreply.github.com>
2024-03-07 20:46:41 -0300

    Calculadora

---
+++ calculadora/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculadora.circunferencia(5));
+        System.out.println(Calculadora.volumeEsfera(3.5));
+        System.out.println(Calculadora.volumeCilindro(15.00354, 9.34));
+    }
+}
